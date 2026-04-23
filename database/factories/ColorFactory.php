c9916c18645599e26c0a318f8122694204575726
Curty Gwendoline <gwendoline.curty@ceff.ch>
2026-04-23 08:06:59 +0200

    new factories for new tables

---
+++ database/factories/ColorFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Color;
+use App\Models\Material;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Color>
+ */
+class ColorFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->safeColorName(), 
+            
+            'id_material' => Material::factory(),
+        ];
+    }
+}
